16f6a3aa8b2b5336e2a2b2de09edd764d940852a
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-05-13 22:53:42 +0300

    Customer service configurations and discovery configurations and Dockerfile added.

--- customerservice/src/main/java/com/turkcell/customerservice/CustomerserviceApplication.java
+++ customerservice/src/main/java/com/turkcell/customerservice/CustomerserviceApplication.java
@@ -2,12 +2,17 @@ package com.turkcell.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CustomerserviceApplication {
+
+	// todo: After completing the authentication server setup, add Spring Security annotations and packages.
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerserviceApplication.class, args);
 	}
 
+	// if needed add kafka listener to the main
 }
