3691a690cf77fb7b893f2662c549731d37b88f0c
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-05-13 23:16:16 +0300

    Order service configurations and discovery and db connection and Dockerfile created.

--- orderservice/src/main/java/com/turkcell/orderservice/OrderserviceApplication.java
+++ orderservice/src/main/java/com/turkcell/orderservice/OrderserviceApplication.java
@@ -2,12 +2,18 @@ package com.turkcell.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderserviceApplication {
+
+	// todo: After completing the authentication server setup, add Spring Security annotations and packages.
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderserviceApplication.class, args);
 	}
+
+	// if needed add kafka listener to the main
 
 }
