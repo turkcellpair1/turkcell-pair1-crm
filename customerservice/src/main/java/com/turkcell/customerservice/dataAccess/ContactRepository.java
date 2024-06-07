a47b20577e9bfab096461825f18aa9ad7d26b250
Yiğit <ygtkly06@gmail.com>
2024-06-08 02:22:13 +0300

    Add contact dto & mapper & manager & controller

--- customerservice/src/main/java/com/turkcell/customerservice/dataAccess/ContactRepository.java
+++ customerservice/src/main/java/com/turkcell/customerservice/dataAccess/ContactRepository.java
@@ -3,5 +3,11 @@ package com.turkcell.customerservice.dataAccess;
 import com.turkcell.customerservice.entities.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ContactRepository extends JpaRepository<Contact,Integer> {
+    Optional<Contact> findByIdAndStatusTrue(int id);
+    Optional<Contact> findByCustomerIdAndStatusTrue(int id);
+    List<Contact> findByStatusTrue();
 }
