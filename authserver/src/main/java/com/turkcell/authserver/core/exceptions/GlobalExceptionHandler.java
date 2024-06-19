f5b919f3c512b478646aa97282a2654acc19cadf
IbrahimSengun63 <67779798+IbrahimSengun63@users.noreply.github.com>
2024-06-19 15:07:05 +0300

    User duplicate email exception handler added and response modified

--- authserver/src/main/java/com/turkcell/authserver/core/exceptions/GlobalExceptionHandler.java
+++ authserver/src/main/java/com/turkcell/authserver/core/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.turkcell.authserver.core.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -24,5 +27,24 @@ public class GlobalExceptionHandler {
         businessExceptionDetails.setDetail(exception.getMessage());
         businessExceptionDetails.setStatus("500");
         return businessExceptionDetails;
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public BusinessExceptionDetails handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
+        BusinessExceptionDetails businessExceptionDetails = new BusinessExceptionDetails();
+        businessExceptionDetails.setDetail(extractEmailFromMessage(exception.getMessage()));
+        businessExceptionDetails.setStatus("500");
+        return businessExceptionDetails;
+    }
+    private String extractEmailFromMessage(String message) {
+        String emailPattern = "Key \\(email\\)=\\(([^)]+)\\)";
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(message);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        return null;
     }
 }
